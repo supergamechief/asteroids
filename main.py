150804e403ebc14f9c2bce4745a151ac16c204e3
supergamechief <mohamedsaad5868@gmail.com>
2024-12-30 14:04:08 +0400

    commit successful

--- main.py
+++ main.py
@@ -10,12 +10,16 @@ def main():
 	print("Screen width: 1280");
 	print("Screen height: 720");
 	
+	clock = pygame.time.Clock()
+	dt = 0
+	
 	while True:
 		for event in pygame.event.get():
     			if event.type == pygame.QUIT:
         			return
 		screen.fill((0,0,0))
 		pygame.display.flip()
+		dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
