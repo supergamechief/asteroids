9f1bef20e045cb0b22fd1f1ca7e5f076d5cb2dd9
supergamechief <mohamedsaad5868@gmail.com>
2024-12-30 14:26:10 +0400

    commit successful

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # this allows us to use code from the open-source pygame library throughout this file
 import pygame
 from constants import *
+from player import Player
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT));
 
@@ -11,13 +12,15 @@ def main():
 	print("Screen height: 720");
 	
 	clock = pygame.time.Clock()
+	player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 	dt = 0
 	
 	while True:
 		for event in pygame.event.get():
     			if event.type == pygame.QUIT:
         			return
-		screen.fill((0,0,0))
+		screen.fill("black")
+		player.draw(screen)
 		pygame.display.flip()
 		dt = clock.tick(60) / 1000
 
